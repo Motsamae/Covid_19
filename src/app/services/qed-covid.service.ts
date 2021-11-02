5fbee7f6e56a771cfde1abd8fbddc544420f6569
Lawrence Phakathi <lawrence.phakathi@qedact.com>
2021-11-02 13:15:05 +0200

    Api service
    Code for the api call on main office on a 7 day period

--- src/app/services/qed-covid.service.ts
+++ src/app/services/qed-covid.service.ts
@@ -15,8 +15,7 @@ export class QedCovidService {
     "params": { country: '', day: '' },
     "headers": {
       "x-rapidapi-host": "covid-193.p.rapidapi.com",
-      "x-rapidapi-key": "d4618b9920msh391054b106bc37ap135d5djsn5feddb02bd93",
-      "useQueryString": "true"
+      "x-rapidAPI-key": "d4618b9920msh391054b106bc37ap135d5djsn5feddb02bd93",
     }
   };
 
@@ -29,7 +28,7 @@ export class QedCovidService {
     this.options.params.country = country;
     this.options.params.day = date;
 
-    return this.http.get(this.options.url + this.options.params + this.options.headers).pipe(
+    return this.http.get(this.options.url, this.options).pipe(
       map((res: any) => {
         this.dailyReportByCountryName[country].push(res.response[0]);
         return res;
@@ -38,18 +37,6 @@ export class QedCovidService {
       }));
   }
 
-  dailyReportByCountryName: any = { 'southAfrica': [],'mauritius':[],'kenya':[],'nigeria':[] }
-  getDailyReportByCountryNameLoop() {
-    // south africa last 7 days
-    // get date    
-    // for (var i = 0; i < 6; i++) {
-      const todaysDate = new Date();
-      todaysDate.setDate(todaysDate.getDate());
-      const todaysDateIso = todaysDate.toISOString().substring(0, 10);
-      const x = this.getDailyReportByCountryName('South Africa', todaysDateIso);
-    // }
-    console.log(x);
-    return this.dailyReportByCountryName;
-  }
+  dailyReportByCountryName: any = { 'south-africa': [], 'mauritius': [], 'kenya': [], 'nigeria': [] };
 
 }
--- src/app/south-africa/south-africa.component.ts
+++ src/app/south-africa/south-africa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
 import { QedCovidService } from '../services/qed-covid.service';
 
 @Component({
@@ -12,15 +13,33 @@ export class SouthAfricaComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.getDailyReportByCountryNameLoop();
+    this.getDailyReportByCountryNameAndDate();
   }
 
-  getDailyReportByCountryNameLoop() {
-    this.qedCovidService.getDailyReportByCountryNameLoop().subscribe(function (data:any) {
-      console.log(data);
-    }, function (err:any) {
-      console.log(err);
-    });
+  getDailyReportByCountryNameAndDate() {
+    const overAllDateList = []
+    for (var i = 0; i < 7; i++) {
+      const todaysDate = new Date();
+      todaysDate.setDate(todaysDate.getDate() - i);
+      const todaysDateIso = todaysDate.toISOString().substring(0, 10);
+      overAllDateList.push(todaysDateIso);
+    }
+    console.log(overAllDateList);
+    forkJoin(this.qedCovidService.getDailyReportByCountryName('south-africa', overAllDateList[0]), this.qedCovidService.getDailyReportByCountryName('south-africa', overAllDateList[1]),
+      this.qedCovidService.getDailyReportByCountryName('south-africa', overAllDateList[2]), this.qedCovidService.getDailyReportByCountryName('south-africa', overAllDateList[3]),
+      this.qedCovidService.getDailyReportByCountryName('south-africa', overAllDateList[4]), this.qedCovidService.getDailyReportByCountryName('south-africa', overAllDateList[5]),
+      this.qedCovidService.getDailyReportByCountryName('south-africa', overAllDateList[6])).subscribe(([dayOne, dayTwo, dayThree, dayFour, dayFive, daySix]:
+        [any, any, any, any, any, any, any]) => {
+        console.log(dayOne);
+        console.log(dayTwo);
+        console.log(dayThree);
+        console.log(dayFour);
+        console.log(dayFive);
+        console.log(daySix);
+        console.log(this.qedCovidService.dailyReportByCountryName);
+      }, function (err: any) {
+        console.log(err);
+      });
   }
 
 }
